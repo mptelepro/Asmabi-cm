55a614dbd5c437a4a5655edd05281e1e818e1839
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-07-30 04:47:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -986,8 +986,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                          )
                      )
                 
-                    Joel_tgx = await query.message.reply_text(
-                        
+                    Joel_tgx = await query.message.reply_photo(
+                        photo=imdb.get('poster'),
                         script.FILE_MSG.format(query.from_user.mention, title, size),
                         parse_mode=enums.ParseMode.HTML,
                         reply_markup=InlineKeyboardMarkup(
