343b4345938d03922a33cf132cc167133c33248c
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-07-29 17:42:39 +0530

    Update update.py

--- plugins/Extra/update.py
+++ plugins/Extra/update.py
@@ -109,7 +109,7 @@ async def up(bot, message):
     user_id = message.from_user.id
     imdb = await get_poster(lg_cd) if IMDB else None
     message_id = message.id
-    name_format = f"StarkBots_{user_id}_{message_id}"
+    name_format = f"https/t.me/KOCHU_KALLAN_RoBOT_{user_id}_{message_id}"
     user_id = message.from_user.id
     
     
