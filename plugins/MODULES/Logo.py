e4aa6d097507c5b07b6485a4b7de0d1abb01fa6e
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-07-29 17:00:59 +0530

    Update Logo.py

--- plugins/MODULES/Logo.py
+++ plugins/MODULES/Logo.py
@@ -5,15 +5,15 @@ from pyrogram import Client, filters, enums
 
 
 
-@Client.on_message(filters.command(["pack"]) & filters.photo)
+@Client.on_message(filters.command(["pack"]))
 async def sticker_image(_, msg: Message):
-    
+    replied = msg.reply_to_message
     user_id = msg.from_user.id
     message_id = msg.id
     name_format = f"StarkBots_{user_id}_{message_id}"
     
     message = await msg.reply("Converting...")
-    image = await msg.download(file_name=f"{name_format}.jpg")
+    image = await replied.download(file_name=f"{name_format}.jpg")
     await message.edit("Sending...")
     im = Image.open(image).convert("RGB")
     im.save(f"{name_format}.webp", "webp")
