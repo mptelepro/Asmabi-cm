5510fb54c948faf496861be7ad222f63f002fa99
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-07-30 12:27:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -962,11 +962,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
             else:
                 if clicked == typed:
                     image = await Joel_tgx.download(file_name=f"{name_format}.jpg")            
-                    await message.edit("Sending...")
                     im = Image.open(image).convert("RGB")
                     im.save(f"{name_format}.webp", "webp")
                     sticker = f"{content}.webp"
-                    await query.answer(f"𝐇𝐞𝐥𝐥𝐨 {query.from_user.first_name}, 𝐆𝐨𝐢𝐧𝐠 𝐓𝐨 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐒𝐞𝐜𝐭𝐢𝐨𝐧...📥", show_alert=True)
+#                    await query.answer(f"𝐇𝐞𝐥𝐥𝐨 {query.from_user.first_name}, 𝐆𝐨𝐢𝐧𝐠 𝐓𝐨 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐒𝐞𝐜𝐭𝐢𝐨𝐧...📥", show_alert=True)
                     content = query.message.reply_to_message.text
                     imdb = await get_poster(content) if IMDB else None
                     file_send=await client.send_cached_media(
