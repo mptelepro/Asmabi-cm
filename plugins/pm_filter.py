0bd5fbd060b293d4b32dfe44a918b46611562fb0
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-07-31 23:23:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -524,8 +524,7 @@ async def filter_language_cb_handler(client: Client, query: CallbackQuery):
         await query.message.delete()
     except MessageNotModified:
         pass
-    await query.answer(f"🏷 𝐓𝐢𝐭𝐥𝐞 : {imdb.get('title')} \n 📆 𝐑𝐞𝐥𝐞𝐚𝐬𝐞 𝐈𝐧𝐟𝐨 : {imdb.get('year')} \n 📀 𝐑𝐮𝐧𝐓𝐢𝐦𝐞 : {imdb.get('runtime')} \n ☀️ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞𝐬 : {imdb.get('languages')} \n\n 🍿{query.message.chat.title}🍿", show_alert=True)
-
+    await query.answer()
 
 # ❤️❤️❤️❤️❤️❤️❤️
 #languages
