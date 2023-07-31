f7cedea1364e48b39a167c0c15d1058885fd57fe
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-01 01:33:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MuraliNew/Auto-search-tamil-bot.git /Auto-search-tamil-bot
+  git clone https://github.com/MuraliNew/Murali-Tamil-Pro.git /Auto-search-tamil-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto-search-tamil-bot
