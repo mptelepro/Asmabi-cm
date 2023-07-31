a7d0ee6555eef74cbca247e00122ca2de81d8b64
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-01 01:40:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -96,7 +96,7 @@ async def give_filter(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         
         k = await message.reply_photo(
-            photo={imdb.get('poster')},
+            photo=imdb.get('poster'),
             caption=f"👋 𝐇𝐞𝐥𝐥𝐨 {message.from_user.mention},\n\n{imdb.get('title')} {imdb.get('year')}  𝐀𝐯𝐚𝐢𝐥𝐚𝐛𝐥𝐞..!!\n\n𝐏𝐥𝐞𝐚𝐬𝐞 𝐉𝐨𝐢𝐧 𝐌𝐲 '𝐔𝐩𝐝𝐚𝐭𝐞𝐬 𝐂𝐡𝐚𝐧𝐧𝐞𝐥' 𝐀𝐧𝐝 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐀𝐠𝐚𝐢𝐧. 😇",
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
