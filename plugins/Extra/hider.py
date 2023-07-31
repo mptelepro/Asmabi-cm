b6a5d9d6c245bf11bbf4be40a34bbce8f28b6b53
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-07-31 17:55:24 +0530

    Update hider.py

--- plugins/Extra/hider.py
+++ plugins/Extra/hider.py
@@ -16,10 +16,12 @@ bot = Client(
 
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot, message):
-	await message.delete()
-	await message.reply.text(okda)
+    await message.delete()
+    await message.reply.text(
+    text=(f"okda")
 	
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot, message):
-	await message.delete()
-        await message.reply.text(okda)
+    await message.delete()
+    await message.reply.text(
+    text=(f"okda")
