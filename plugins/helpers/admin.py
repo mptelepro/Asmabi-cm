6977a2503af4314f765a71d5852bf9647957af55
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-07-31 11:56:44 +0530

    Update admin.py

--- plugins/helpers/admin.py
+++ plugins/helpers/admin.py
@@ -18,7 +18,7 @@ from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
 
 
 ADMINS = int(os.environ.get("ADMINS"))
-DATABASE = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 DEFAULT_LANGUAGE = os.environ.get("DEFAULT_LANGUAGE", "en")
 
 class Database:
