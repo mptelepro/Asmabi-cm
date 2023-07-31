0f5eaf4fc49d6032f5bf9bd6eed2d5d7939dde39
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-07-31 12:51:01 +0530

    Update vars.py

--- plugins/helpers/vars.py
+++ plugins/helpers/vars.py
@@ -1,6 +1,6 @@
 import os
 
 
-BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+ADMINS = int(os.environ.get("ADMINS"))
 DATABASE = os.environ.get("DATABASE_URL")
 DEFAULT_LANGUAGE = os.environ.get("DEFAULT_LANGUAGE", "en")
