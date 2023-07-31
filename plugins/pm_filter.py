ce66cf7380e2d598be6f784f1edc5f4bf351a61e
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-07-31 22:54:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -423,7 +423,7 @@ async def filter_language_cb_handler(client: Client, query: CallbackQuery):
         await query.answer(f"🏷 𝐓𝐢𝐭𝐥𝐞 : {imdb.get('title')} \n 📆 𝐑𝐞𝐥𝐞𝐚𝐬𝐞 𝐈𝐧𝐟𝐨 : {imdb.get('year')} \n 📀 𝐑𝐮𝐧𝐓𝐢𝐦𝐞 : {imdb.get('runtime')} \n ☀️ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞𝐬 : {imdb.get('languages')} \n\n 🍿{query.message.chat.title}🍿", show_alert=True)
         return
     else:      
-        await query.answer(f"⚠️𝐇𝐞𝐲 {query.from_user.first_name}, {search}𝐅𝐢𝐥𝐞𝐬 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠.. \n\n ⏳️𝐅𝐢𝐥𝐞𝐬 {offset}", show_alert=True)
+        await query.answer(f"⚠️𝐇𝐞𝐲 {query.from_user.first_name}, {search}𝐅𝐢𝐥𝐞𝐬 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠..", show_alert=True)
     
 
     files, offset, total_results = await get_search_results(chat_id, search, offset=0, filter=True)
