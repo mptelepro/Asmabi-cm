3ff88e56dc3d58e421cbf23da175d27950dd84fc
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-07-31 12:53:55 +0530

    Update vars.py

--- plugins/helpers/vars.py
+++ plugins/helpers/vars.py
@@ -2,5 +2,5 @@ import os
 
 
 ADMINS = int(os.environ.get("ADMINS"))
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE = os.environ.get("DATABASE_URL")
 DEFAULT_LANGUAGE = os.environ.get("DEFAULT_LANGUAGE", "en")
