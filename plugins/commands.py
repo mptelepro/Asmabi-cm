2c576ab7d2881a3725e28a470ff4129ea747ce38
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-01 12:49:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -420,7 +420,8 @@ async def start(client, message):
             reply_markup=InlineKeyboardMarkup(btn)
         )
         return
-    msg = await client.send_cached_media(
+     chat_id = temp.SHORT.get(user)
+     msg = await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
