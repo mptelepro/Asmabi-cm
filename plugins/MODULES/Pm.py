e3255f3ff45882a1d23a65a714505d23ff0cd49b
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-03 04:58:36 +0530

    Update Pm.py

--- plugins/MODULES/Pm.py
+++ plugins/MODULES/Pm.py
@@ -182,37 +182,31 @@ async def replay_media(client: Client, message):
 
 
 
-@Client.on_message(filters.private & filters.media & filters.reply)
+@Client.on_message(filters.private & filters.media)
 async def reply_media(client: Client, message):
-    try:
-        reference_id = True
-        if message.reply_to_message is not None:
-            file = message.reply_to_message
-            try:
-                reference_id = file.text.split()[2]
-            except Exception:
-                pass
-            try:
-                reference_id = file.caption.split()[2]
-            except Exception:
-                pass
-            await client.copy_message(
-                chat_id=ADMIN,
-                from_chat_id=message.chat.id,
-                message_id=message.id,
-                parse_mode=enums.ParseMode.HTML,
-            reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton('🎁𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬🎁', url="http://t.me/nasrani_bot?startgroup=true")
-                            ],
-                            [
-                                InlineKeyboardButton('📩𝐑𝐄𝐐𝐔𝐀𝐒𝐓 𝐆𝐑𝐎𝐔𝐏📩', url="https://t.me/NasraniMovies"),
-                                InlineKeyboardButton('☘𝐍𝐄𝐖 𝐌𝐎𝐕𝐈𝐄𝐒☘', url="https://t.me/HDAZmovies")
-                            ]                            
-                        ]
-                    )
-                )        
+    
+    if message.from_user.id in ADMINS:
+        await replay_media(bot, message)
+        return
+    info = await bot.get_users(user_ids=message.from_user.id)
+    reference_id = int(message.chat.id)
+    await client.copy_message(
+        chat_id=ADMIN,
+        from_chat_id=message.chat.id,
+        message_id=message.id,
+        parse_mode=enums.ParseMode.HTML,
+        reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton('🎁𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬🎁', url="http://t.me/nasrani_bot?startgroup=true")
+                    ],
+                    [
+                        InlineKeyboardButton('📩𝐑𝐄𝐐𝐔𝐀𝐒𝐓 𝐆𝐑𝐎𝐔𝐏📩', url="https://t.me/NasraniMovies"),
+                        InlineKeyboardButton('☘𝐍𝐄𝐖 𝐌𝐎𝐕𝐈𝐄𝐒☘', url="https://t.me/HDAZmovies")
+                    ]                            
+                ]
+            )
+        )        
     except Exception as e:
         logger.exception(e)
 
