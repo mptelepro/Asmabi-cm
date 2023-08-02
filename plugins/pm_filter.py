f20c5d7d9891976a2cb735ffa4c882bd22009d2a
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-02 14:45:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2606,7 +2606,8 @@ async def auto_filter(client, msg, spoll=False):
             return
     else:
         message = msg.message.reply_to_message  # msg will be callback query
-        search, files, offset, total_results = spoll
+        req, key, search, files, offset, total_results = spoll
+#        search, files, offset, total_results = spoll
         m=await message.reply_text(f"<b><i> 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
         settings = await get_settings(message.chat.id)
         await msg.message.delete()
