17e58afa1eac1ef547be8b1e19ab2037869e291f
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-03 17:47:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -380,11 +380,14 @@ async def advantage_spoll_choker(bot, query):
                 k = (movie, files, offset, total_results)
                 await auto_filter(bot, query, k)
             else:
-                reqstr1 = query.from_user.id if query.from_user else 0
-                reqstr = await bot.get_users(reqstr1)
+		
+#                reqstr1 = query.from_user.id if query.from_user else 0
+#                reqstr = await bot.get_users(reqstr1)
                 if NO_RESULTS_MSG:
 #                    await bot.send_message(chat_id=ADMIN, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
 #                    k = await query.message.edit(script.MVE_NT_FND)
+		    info = await bot.get_users(user_ids=query.message.from_user.id)
+                    reference_id = int(query.message.chat.id)
                     buttons = [[
                         InlineKeyboardButton("📢 Updates Channel 📢", url = k.link)
                     ],[
@@ -401,8 +404,7 @@ async def advantage_spoll_choker(bot, query):
 #		    k = await query.message.reply_text(script.MVE_NT_FND)
                     await asyncio.sleep(190)
                     await m.delete()
-                except Exception as e:
-                    logger.exception(e)
+                
 
 
 
