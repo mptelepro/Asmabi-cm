6817038f8fc9f22124eb494bc5fffee6f80beca0
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-03 17:11:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -379,10 +379,23 @@ async def advantage_spoll_choker(bot, query):
                 reqstr1 = query.from_user.id if query.from_user else 0
                 reqstr = await bot.get_users(reqstr1)
                 if NO_RESULTS_MSG:
-                    await bot.send_message(chat_id=ADMIN, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
-                k = await query.message.edit(script.MVE_NT_FND)
-                await asyncio.sleep(10)
-                await k.delete()
+#                    await bot.send_message(chat_id=ADMIN, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
+                    k = await query.message.edit(script.MVE_NT_FND)
+                    buttons = [[
+                        InlineKeyboardButton("📢 Updates Channel 📢", url = k.link)
+                    ],[
+                    InlineKeyboardButton("🔁 Request Again 🔁", callback_data="grp_checksub")
+                    ]]
+                    reply_markup = InlineKeyboardMarkup(buttons)
+                    m = await bot.send_photo(
+		            chat_id=ADMIN,
+                        photo=random.choice(PICS),
+                        caption=f"👋 𝐇𝐞𝐥𝐥𝐨 {message.from_user.mention}, {movie} {message.chat.title}\n\n..!!\n\n𝐏𝐥𝐞𝐚𝐬𝐞 𝐉𝐨𝐢𝐧 𝐌𝐲 '𝐔𝐩𝐝𝐚𝐭𝐞𝐬 𝐂𝐡𝐚𝐧𝐧𝐞𝐥' 𝐀𝐧𝐝 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐀𝐠𝐚𝐢𝐧. 😇",
+                        reply_markup=reply_markup,
+                    parse_mode=enums.ParseMode.HTML
+                    )
+                    await asyncio.sleep(10)
+                    await k.delete()
                 
 
 
