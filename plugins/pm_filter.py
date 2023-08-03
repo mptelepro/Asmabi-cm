f3475af32de471d23f0ea8cf7648b31387d773b3
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-03 17:32:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2854,26 +2854,7 @@ async def advantage_spell_chok(client, msg):
     await asyncio.sleep(60)
     await spell_check_del.delete()
     await msg.delete()
-    reqstr1 = msg.from_user.id if msg.from_user else 0
-    reqstr = await client.get_users(reqstr1)
-    if NO_RESULTS_MSG:
-#        await bot.send_message(chat_id=msg.chat.id, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
-#        k = await .msg.edit(script.MVE_NT_FND)
-        buttons = [[
-            InlineKeyboardButton("📢 Updates Channel 📢", url = k.link)
-        ],[
-            InlineKeyboardButton("🔁 Request Again 🔁", callback_data="grp_checksub")
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        m = await client.send_photo(
-	    chat_id=ADMIN,
-            photo=random.choice(PICS),
-            caption=f"👋 𝐇𝐞𝐥𝐥𝐨 {message.from_user.mention}, {movie} {message.chat.title}\n\n..!!\n\n𝐏𝐥𝐞𝐚𝐬𝐞 𝐉𝐨𝐢𝐧 𝐌𝐲 '𝐔𝐩𝐝𝐚𝐭𝐞𝐬 𝐂𝐡𝐚𝐧𝐧𝐞𝐥' 𝐀𝐧𝐝 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐀𝐠𝐚𝐢𝐧. 😇",
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-            )
-        await asyncio.sleep(10)
-        await k.delete()
+    
 #    except Exception as e:
 #        await m.edit(e)        
 #        e = traceback.format_exc()
