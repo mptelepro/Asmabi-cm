39929fe71adde648410ded6dd75b874c1119a2a4
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-07 18:11:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2381,14 +2381,14 @@ async def auto_filter(client, msg, spoll=False):
                      text=f"{' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}",
                      callback_data=f'{pre}#{file.file_id}'
                  ),
-#             ],
+             ],
              [
                  InlineKeyboardButton(
                      text=f"{get_size(file.file_size)}",
                      callback_data=f'{pre}#{file.file_id}'
                  )
              ]
-             for file in files
+#             for file in files
          ]
              
         
