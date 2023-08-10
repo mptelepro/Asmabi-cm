4309fe63aaf3b673be4ef9d1a237faaafcdf4b4a
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-10 14:54:24 +0530

    Update Pm.py

--- plugins/MODULES/Pm.py
+++ plugins/MODULES/Pm.py
@@ -115,7 +115,8 @@ async def reply_text(client: Client, message):
                 pass
             await client.send_message(
                 text=message.text,
-                chat_id=int(reference_id),
+                chat_id=message.reply_to_message.from_user.id,
+#		chat_id=int(reference_id),
                 parse_mode=enums.ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
                         [
