552728f5761031afc623920911bdf1398b9f098a
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-11 18:50:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1757,7 +1757,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 text=f"<b>𝐇𝐞𝐥𝐥𝐨 {query.message.reply_to_message.from_user.mention} {text} 𝐌𝐨𝐯𝐢𝐞 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝.</b>",
                 reply_markup=reply_markup,
                 disable_web_page_preview=True,
-                parse_mode=enums.ParseMode.HTML
+                parse_mode=enums.ParseMode.HTML,
                 reply_to_message_id=query.message.id
             )
 #            await asyncio.sleep(60)
