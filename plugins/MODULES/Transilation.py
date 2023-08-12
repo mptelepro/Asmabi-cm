d1339ac97c824d58b29302fa15c97613e4473855
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-12 05:37:55 +0530

    Update Transilation.py

--- plugins/MODULES/Transilation.py
+++ plugins/MODULES/Transilation.py
@@ -84,9 +84,9 @@ async def translate(update, text):
 async def left(client,message):
 	if (message.reply_to_message):
 		try:
-#			lgcd = message.text.split("/tr")
-#			lg_cd = lgcd[1].lower().replace(" ", "")
-			lg_cd = message.text.split(" ")
+			lgcd = message.text.split("/tr")
+			lg_cd = lgcd[2].lower().replace(" ", " ")
+#			lg_cd = message.text.split(" ")
 #  		        lg_cd = message.text.split(None, 1)[1]
 			tr_text = message.reply_to_message.text
 			fromt = i
