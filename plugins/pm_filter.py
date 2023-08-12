de84f0bea2d1f92e4539db2dfd51acd404ca8d11
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-13 01:59:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1964,7 +1964,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         query_id = query.message.chat.id
         content = query.message.text
         imdb = await get_poster(content) if IMDB else None
-        k await query.message.reply_text(text=f"🏷 𝐓𝐢𝐭𝐥𝐞 : {imdb.get('title')} \n 📆 𝐑𝐞𝐥𝐞𝐚𝐬𝐞 𝐈𝐧𝐟𝐨 : {imdb.get('year')} \n 📀 𝐑𝐮𝐧𝐓𝐢𝐦𝐞 : {imdb.get('runtime')} \n ☀️ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞𝐬 : {imdb.get('languages')} \n\n 🍿{query.message.chat.title}🍿")
+        k = await query.message.reply_text(text = f"🏷 𝐓𝐢𝐭𝐥𝐞 : {imdb.get('title')} \n 📆 𝐑𝐞𝐥𝐞𝐚𝐬𝐞 𝐈𝐧𝐟𝐨 : {imdb.get('year')} \n 📀 𝐑𝐮𝐧𝐓𝐢𝐦𝐞 : {imdb.get('runtime')} \n ☀️ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞𝐬 : {imdb.get('languages')} \n\n 🍿{query.message.chat.title}🍿")
         query_id = query.message.chat.id
         content = query.message.text
         imdb = await get_poster(k) if IMDB else None
