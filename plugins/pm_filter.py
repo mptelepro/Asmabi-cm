3406b44c39b83ebf3bf2c3e9e9d97db07640cb16
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-13 13:03:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1736,19 +1736,30 @@ async def cb_handler(client: Client, query: CallbackQuery):
         mv_rqst = query.message.text
         reqstr1 = query.message.from_user.id if query.message.from_user else 0
         reqstr = await client.get_users(reqstr1)
-        for k, movie in enumerate(movielist):
-            btn = [[
-                InlineKeyboardButton(
-                    text=movie.strip(),
-                    callback_data=f"spolling#{reqstr1}#{k}",
-                 )
-            ]]# for k, movie in enumerate(movielist)]
+        movielist = []
+        gs_parsed = list(dict.fromkeys(gs_parsed))  # removing duplicates https://stackoverflow.com/a/7961425
+        if len(gs_parsed) > 3:
+            gs_parsed = gs_parsed[:3]
+        if gs_parsed:
+            for mov in gs_parsed:
+                imdb_s = await get_poster(mov.strip(), bulk=True)  # searching each keyword in imdb
+                if imdb_s:
+                    movielist += [movie.get('title') for movie in imdb_s]
+                movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
+                movielist = list(dict.fromkeys(movielist))
+                for k, movie in enumerate(movielist):
+                    btn = [[
+                        InlineKeyboardButton(
+                            text=movie.strip(),
+                            callback_data=f"spolling#{reqstr1}#{k}",
+                        )
+                    ]]# for k, movie in enumerate(movielist)]
 #            btn.append([InlineKeyboardButton(text="Close", callback_data=f'spol#{reqstr1}#close_spellcheck')])
-            spell_check_del = await query.message.reply_photo(
-                photo=SPELL_IMG,
-                caption=(script.CUDNT_FND.format(mv_rqst)),
-                reply_markup=InlineKeyboardMarkup(btn)
-            )
+                    spell_check_del = await query.message.reply_photo(
+                        photo=SPELL_IMG,
+                        caption=(script.CUDNT_FND.format(mv_rqst)),
+                        reply_markup=InlineKeyboardMarkup(btn)
+                    )
 
 
 
