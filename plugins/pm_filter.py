4a3f19283829ff79c8123018aabb864513ce451e
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-13 14:46:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1744,32 +1744,32 @@ async def cb_handler(client: Client, query: CallbackQuery):
         find = search.split(" ")
         search = ""
         removes = ["in","upload", "series", "full", "horror", "thriller", "mystery", "print", "file"]
-            for x in find:
-                if x in removes:
-                    continue
-                else:
-                    search = search + x + " "
-            search = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|bro|bruh|broh|helo|that|find|dubbed|link|venum|iruka|pannunga|pannungga|anuppunga|anupunga|anuppungga|anupungga|film|undo|kitti|kitty|tharu|kittumo|kittum|movie|any(one)|with\ssubtitle(s)?)", "", search, flags=re.IGNORECASE)
-            search = re.sub(r"\s+", " ", search).strip()
-            search = search.replace("-", " ")
-            search = search.replace(":","")
-            files, offset, total_results = await get_search_results(query.message.chat.id ,search, offset=0, filter=True)
-            settings = await get_settings(message.chat.id)
-            pre = 'filep' if settings['file_secure'] else 'file'
-            key = f"{query.message.chat.id}-{query.message.id}"
-            FRESH[key] = search
-            temp.GETALL[key] = files
-            temp.SHORT[query.message.from_user.id] = message.chat.id
+        for x in find:
+            if x in removes:
+                continue
+            else:
+                search = search + x + " "
+        search = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|bro|bruh|broh|helo|that|find|dubbed|link|venum|iruka|pannunga|pannungga|anuppunga|anupunga|anuppungga|anupungga|film|undo|kitti|kitty|tharu|kittumo|kittum|movie|any(one)|with\ssubtitle(s)?)", "", search, flags=re.IGNORECASE)
+        search = re.sub(r"\s+", " ", search).strip()
+        search = search.replace("-", " ")
+        search = search.replace(":","")
+        files, offset, total_results = await get_search_results(query.message.chat.id ,search, offset=0, filter=True)
+        settings = await get_settings(message.chat.id)
+        pre = 'filep' if settings['file_secure'] else 'file'
+        key = f"{query.message.chat.id}-{query.message.id}"
+        FRESH[key] = search
+        temp.GETALL[key] = files
+        temp.SHORT[query.message.from_user.id] = message.chat.id
     
-            if settings["button"]:
-                btn = [
-                    [
-                        InlineKeyboardButton(
-                            text=f"{random.choice(RUN_STRINGS)}[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}", callback_data=f'{pre}#{file.file_id}'
-                        ),
-                    ]
-                    for file in files
+        if settings["button"]:
+            btn = [
+                [
+                    InlineKeyboardButton(
+                        text=f"{random.choice(RUN_STRINGS)}[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}", callback_data=f'{pre}#{file.file_id}'
+                    ),
                 ]
+                for file in files
+            ]
         
 
 
