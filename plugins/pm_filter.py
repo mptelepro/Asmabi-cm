90d1a48eac7b791d352d46cf075d13169028bffa
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-13 14:13:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1739,7 +1739,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         settings = await get_settings(query.message.chat.id)
         query = re.sub(
             r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
-            "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
+            "", query.message.reply_to_message.text, flags=re.IGNORECASE)  # plis contribute some common words
         query = query.strip() + " movie"
         find = mv_rqst.replace(" ", "+")
         query = ""
