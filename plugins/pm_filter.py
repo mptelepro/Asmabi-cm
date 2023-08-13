906fc801fdd329ff4b51417e302444ff14c8f989
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-13 16:11:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1807,7 +1807,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             btn.append(
                 [InlineKeyboardButton(text="𝐍𝐎 𝐌𝐎𝐑𝐄 𝐏𝐀𝐆𝐄𝐒 𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄",callback_data="pages")]
             ) 
-        imdb = imdb = await get_poster(search) if IMDB else None
+        imdb = imdb = await get_poster(search, file=(files[0]).file_name) if IMDB else None
         
         cur_time = datetime.now(pytz.timezone('Asia/Kolkata')).time()
         time_difference = timedelta(hours=cur_time.hour, minutes=cur_time.minute, seconds=(cur_time.second+(cur_time.microsecond/1000000))) - timedelta(hours=curr_time.hour, minutes=curr_time.minute, seconds=(curr_time.second+(curr_time.microsecond/1000000)))
