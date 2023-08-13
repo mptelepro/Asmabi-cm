1758a3245b15336bdbb4046c954de562040b40d9
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-13 21:53:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1865,7 +1865,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         imdb = await get_poster(conten) if IMDB else None
         
         
-        _, ident, from_user = query.data.split("#")
+        ident, from_user = query.data.split("#")
         link = await client.create_chat_invite_link(int(query.message.chat.id))
         k = await query.answer(f"🏷 𝐓𝐢𝐭𝐥𝐞 : {imdb.get('title')} \n 📆 𝐑𝐞𝐥𝐞𝐚𝐬𝐞 𝐈𝐧𝐟𝐨 : {imdb.get('year')} \n 📀 𝐑𝐮𝐧𝐓𝐢𝐦𝐞 : {imdb.get('runtime')} \n ☀️ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞𝐬 : {imdb.get('languages')} \n\n 🍿{query.message.chat.title}🍿", show_alert=True)
 
