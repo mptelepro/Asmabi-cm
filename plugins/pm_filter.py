ae94a2846a3bae08453aa5088ad075f8557b0f84
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-14 17:40:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1198,24 +1198,24 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     b_msg = script.DONE_MSG.format(query.from_user.mention, title, size)
                     async for user in users:
                 
-                    pti, sh = await broadcast_messages(int(user['id']), b_msg)
-                    if pti:
-                        success += 1
-                    elif pti == False:
-                        if sh == "Blocked":
-                            blocked+=1
-                        elif sh == "Deleted":
-                            deleted += 1
-                        elif sh == "Error":
-                            failed += 1
-                    done += 1
-                    await asyncio.sleep(2)
-                    if not done % 20:
+                        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+                        if pti:
+                            success += 1
+                        elif pti == False:
+                            if sh == "Blocked":
+                                blocked+=1
+                            elif sh == "Deleted":
+                                deleted += 1
+                            elif sh == "Error":
+                                failed += 1
+                        done += 1
+                        await asyncio.sleep(2)
+                        if not done % 20:
                 
-                        sp = await sts.edit(f"Broadcast Completed:\nCompleted in  seconds.\n\nTotal Users {total_users}\nCompleted:")
+                            sp = await sts.edit(f"Broadcast Completed:\nCompleted in  seconds.\n\nTotal Users {total_users}\nCompleted:")
 
                         
-                        await sp.delete()
+                            await sp.delete()
 
                     
                 else:
