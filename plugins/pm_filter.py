2c29f02dc5e3cc4e27ec4239b8adc17f4bfb08e8
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-14 15:50:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1948,11 +1948,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,                       
             )
             users = await db.get_all_users()
-#            b_msg = message.reply_to_message
+            b_msg = sp
             sts = await query.message.reply_text(
             text='Broadcasting your messages...'
             )
-#            start_time = time.time()
+            start_time = time.time()
             total_users = await db.total_users_count()
             done = 0
             blocked = 0
@@ -1969,15 +1969,31 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ]]
                 reply_markup = InlineKeyboardMarkup(buttons)
            
-                pti, sh = await client.send_sticker(
+                pti, sh = await broadcast_messages(
                 chat_id=int(user['id']),
-                sticker=sticker,            
+                b_msg,            
                 reply_markup=reply_markup,                       
                 )
-                
-                await m.delete()
-                await asyncio.sleep(600)
-                await k.delete()
+                if pti:
+                    success += 1
+                elif pti == False:
+                    if sh == "Blocked":
+                        blocked+=1
+                    elif sh == "Deleted":
+                        deleted += 1
+                    elif sh == "Error":
+                        failed += 1
+                done += 1
+                await asyncio.sleep(2)
+                if not done % 20:
+                    await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+            time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+            await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+
+            await m.delete()
+            await asyncio.sleep(600)
+            await k.delete()                
+            
             
 
 #            await query.message.edit_reply_markup(reply_markup)
