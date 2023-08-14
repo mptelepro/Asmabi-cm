983fcc737c4dcc580c17e0f2c310092582762ce1
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-14 17:06:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2686,7 +2686,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
-    await query.answer(MSG_ALRT)
+#    await query.answer(MSG_ALRT)
 
     
 async def auto_filter(client, msg, spoll=False):
