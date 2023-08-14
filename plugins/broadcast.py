9d3d32f8e0e6999be24b84174655e3e129963c7b
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-14 15:32:25 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -24,8 +24,7 @@ async def verupikkals(bot, message):
 
     success = 0
     async for user in users:
-        pti, sh = await broadcast_messages(
-        int(user['id']), b_msg)
+        pti, sh = await broadcast_messages(int(user['id']), b_msg)
         if pti:
             success += 1
         elif pti == False:
