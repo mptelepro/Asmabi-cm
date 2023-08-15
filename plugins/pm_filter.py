323e4f04e567b48c06ff796b5a35f2267984686d
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-15 20:21:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2817,9 +2817,9 @@ async def auto_filter(client, msg, spoll=False):
         else:
             # cap = f"<b>Hᴇʏ {message.from_user.mention}, Hᴇʀᴇ ɪs ᴛʜᴇ ʀᴇsᴜʟᴛ ғᴏʀ ʏᴏᴜʀ ᴏ̨ᴜᴇʀʏ {search} \n\n</b>"
             cap = f"<b>Hᴇʏ {message.from_user.mention}, Fᴏᴜɴᴅ {total_results} Rᴇsᴜʟᴛs ғᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {search}\n\n</b>"
-            cap+="𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐅𝐢𝐥𝐞𝐬  <b> <a href='https://telegra.ph/{search}'>https://telegra.ph/{cap}</a> </b>"
+            cap+="<b> <a href='https://telegra.ph/{search}'>{cap}</a> </b> \n\n"
             for file in files:
-                cap += f"<b>{random.choice(RUN_STRINGS)} <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
+                cap += f"<b> <a href='https://telegra.ph/{cap}'>{search}</a> </b>"
 
     if imdb and imdb.get('poster'):
         try:
