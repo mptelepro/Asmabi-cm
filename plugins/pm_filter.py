6a4d439ea96bd17c8aeccab63eb743d8f721678b
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-15 22:07:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2820,7 +2820,7 @@ async def auto_filter(client, msg, spoll=False):
             cap+="<b><u> 𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐅𝐢𝐥𝐞𝐬 </u></b>\n\n"
             for file in files:
                 cap += f"<b> {random.choice(RUN_STRINGS)} <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
-            cap = f" <a href='https://telegra.ph/{cap}'>{search}</a> </b>"
+            cap += f" <a href='https://telegra.ph/{cap}'>{search}</a> </b>"
         
     if imdb and imdb.get('poster'):
         try:
