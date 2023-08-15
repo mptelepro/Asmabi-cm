96ccb87f4d54c4447090dcb2b452f4bc01756af6
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-15 17:43:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2773,25 +2773,25 @@ async def auto_filter(client, msg, spoll=False):
 #        ]
  #       ]
        btn = [ [
-                [
-                    InlineKeyboardButton(
+           [
+               InlineKeyboardButton(
                         
-                        text = f"<b>{random.choice(RUN_STRINGS)} <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
-                    ),
-                ],
-                [
-                    InlineKeyboardButton(
-                        text = f"<b>{random.choice(RUN_STRINGS)} <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
-                    ),
-                ]
+               text = f"<b>{random.choice(RUN_STRINGS)} <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
+               ),
+            ],
+            [
+                InlineKeyboardButton(
+                text = f"<b>{random.choice(RUN_STRINGS)} <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
+                ),
+            ]
                 
-            ] for file in files]
-#        btn.append([InlineKeyboardButton(text="Close", callback_data='close_data')])
-#        spell_check_del = await msg.reply_photo(
-#            photo=SPELL_IMG,
-#            caption=(script.CUDNT_FND.format(mv_rqst)),
-#            reply_markup=InlineKeyboardMarkup(btn)
-#        )
+        ] for file in files]
+        btn.append([InlineKeyboardButton(text="Close", callback_data='close_data')])
+        spell_check_del = await msg.reply_photo(
+            photo=SPELL_IMG,
+            caption=(script.CUDNT_FND.format(mv_rqst)),
+            reply_markup=InlineKeyboardMarkup(btn)
+        )
 
 #                btn.insert(0, 
 #                    [
