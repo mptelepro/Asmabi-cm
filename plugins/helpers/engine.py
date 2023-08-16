bb087e5e2ee32d901ddde1e7a36d421824b133d5
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-16 14:11:29 +0530

    Update engine.py

--- plugins/helpers/engine.py
+++ plugins/helpers/engine.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters, enums
 
 import openai
-from info import OPENAI
+from info import OPENAI, ADMINS
 async def ai(query):
     
     openai.api_key = OPENAI #Your openai api key
@@ -20,4 +20,25 @@ async def ask_ai(client, m, message):
         error_message = f"An error occurred: {e}"
         await m.edit(error_message)
     else:
-        await message.reply_text(f"😥 Sᴏʀʀʏ {message.from_user.mention}, \nYᴏᴜ Cᴀɴ'ᴛ Aꜱᴋ Qᴜᴇꜱᴛɪᴏɴꜱ Hᴇʀᴇ !!!\n/openai Cᴏᴍᴍᴀɴᴅ Oɴʟʏ Wᴏʀᴋ Oɴ Mʏ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ ♨️")
+        content = message.text
+        user = message.from_user.first_name
+        user_id = message.from_user.id
+        if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
+        if user_id in ADMINS: return
+            await message.reply_text(f"😥 Sᴏʀʀʏ {message.from_user.mention}, \nYᴏᴜ Cᴀɴ'ᴛ Aꜱᴋ Qᴜᴇꜱᴛɪᴏɴꜱ Hᴇʀᴇ !!!\n/openai Cᴏᴍᴍᴀɴᴅ Oɴʟʏ Wᴏʀᴋ Oɴ Mʏ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ ♨️")
+
+
+
+
+@Client.on_message(filters.chat(SUPPORT_CHAT) & filters.text & filters.incoming)
+async def pm_text(bot, message):
+    content = message.text
+    user = message.from_user.first_name
+    user_id = message.from_user.id
+    if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
+    if user_id in ADMINS: return # ignore admins
+    await message.reply_text("<b>Yᴏᴜʀ ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ sᴇɴᴛ ᴛᴏ ᴍʏ ᴍᴏᴅᴇʀᴀᴛᴏʀs !</b>")
+    await bot.send_message(
+        chat_id=LOG_CHANNEL,
+        text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
+    )
