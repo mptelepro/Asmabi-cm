a7d4436d45961f3e4c54d070f8e9f9f52ee7a36c
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-16 15:04:32 +0530

    Update openai.py

--- plugins/Extra/openai.py
+++ plugins/Extra/openai.py
@@ -11,3 +11,8 @@ async def openai_ask(client, message):
        return await message.reply_text("Give an input!")
     m = await message.reply_text("👀")
     await ask_ai(client, m, message)
+
+
+@Client.on_message(filters.text & filters.command('openai'))
+async def openai_ask(client, message):
+    await message.reply_text(f"😥 Sᴏʀʀʏ {message.from_user.mention},\nYᴏᴜ Cᴀɴ'ᴛ Aꜱᴋ Qᴜᴇꜱᴛɪᴏɴꜱ Hᴇʀᴇ !!!\n/openai Cᴏᴍᴍᴀɴᴅ Oɴʟʏ Wᴏʀᴋ Oɴ Mʏ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ ♨️")
