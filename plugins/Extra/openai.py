f96416f3fbfc0e18bd0af4c7aa0b964bfa8506e1
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-16 15:12:11 +0530

    Update openai.py

--- plugins/Extra/openai.py
+++ plugins/Extra/openai.py
@@ -7,13 +7,15 @@ support = "https://t.me/NASRANI_SUPPORT"
 
 @Client.on_message(filters.chat(-1001203428484) & filters.text & filters.command('openai'))
 async def openai_ask(client, message):
+    chat_type = message.chat.type
+    if chat_type == message.chat.id:
+        return await message.reply_text("This Command Work Only in group\n\nTry it in your own group")
+#    elif chat_type in -1001203428484:
     if len(message.command) == 1:
        return await message.reply_text("Give an input!")
     m = await message.reply_text("👀")
-        await ask_ai(client, m, message)
-    else:
-        
-        await message.reply_text(f"😥 Sᴏʀʀʏ {message.from_user.mention}, \nYᴏᴜ Cᴀɴ'ᴛ Aꜱᴋ Qᴜᴇꜱᴛɪᴏɴꜱ Hᴇʀᴇ !!!\n/openai Cᴏᴍᴍᴀɴᴅ Oɴʟʏ Wᴏʀᴋ Oɴ Mʏ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ ♨️")
+    await ask_ai(client, m, message)
 
+    
 
 
