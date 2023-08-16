30c40668d44777ec31e6cb197540689bcfa2849d
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-16 16:14:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -86,13 +86,9 @@ RUN_STRINGS = (
 #    return audio
 
 
-@Client.on_message(filters.command("openai") & filters.text)
+@Client.on_message(filters.command("openai"))
 async def pm_text(bot, message):
-    content = message.text
-    user = message.from_user.first_name
-    user_id = message.from_user.id
-    if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
-    if user_id in ADMINS: return # ignore admins
+
     await message.reply_text("<b>Yᴏᴜʀ ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ sᴇɴᴛ ᴛᴏ ᴍʏ ᴍᴏᴅᴇʀᴀᴛᴏʀs !</b>")
     
 
