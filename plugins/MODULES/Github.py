9c8e31e6183c04503538b9338c0be1919f0191d1
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-17 00:03:01 +0530

    Update Github.py

--- plugins/MODULES/Github.py
+++ plugins/MODULES/Github.py
@@ -12,15 +12,6 @@ from pyrogram import Client, filters
 BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('✨ Made By ✨', url='https://t.me/nasrani_update')]])
 A = """{} with user id:- {} used /git command."""
 
-@Client.on_message(filters.command(["repo", "repository"]))
-async def repo(bot, message):
-    if len(message.command) != 2:
-        await message.reply_text("/github Username \n\n Like:- `/github hkrrish`", quote=True)
-        return
-    await message.reply_chat_action(enums.ChatAction.TYPING)
-    k = await message.reply_text("**Processing...⏳**", quote=True)    
-    un = message.text.split(None, 1)[1]
-    await message.reply_text(f'https://github.com/search?q={un}+language%3APython&type=repositories&l=Python&s=updated&o=desc')
     
 @Client.on_message(filters.command(["github", "git"]))
 async def getgithub(bot, message):
@@ -62,43 +53,54 @@ async def getgithub(bot, message):
     await bot.send_message(LOG_CHANNEL, A.format(message.from_user.mention, message.from_user.id)) 
     await k.delete()
         
-@Client.on_message(filters.command(["myr", "poory"]))
-async def myr(bot, message):
-    if len(message.command) != 2:
-        await message.reply_text("/github Username \n\n Like:- `/github hkrrish`", quote=True)
-        return
-    await message.reply_chat_action(enums.ChatAction.TYPING)
-    k = await message.reply_text("**Processing...⏳**", quote=True)    
+@Client.on_message(filters.chat(-1001203428484) & filters.text & filters.command('repo'))
+async def repo(Kashmira, message):
     un = message.text.split(None, 1)[1]
+    search = https://github.com/search?q={un}+language%3APython&type=repositories&l=Python&s=updated&o=desc
+    
     repositories = requests.get('https://api.github.com/search/repositories')
-    data = json.loads(response.text)
-    result = request.json()
-    title = repo['name']
-    url = result['html_url']
-    name = result['name']
-    company = result['company']
-    bio = result['bio']
-    created_at = result['created_at']
-    avatar_url = result['avatar_url']
-    blog = result['blog']
-    location = result['location']
-    repositories = result['public_repos']
-    followers = result['followers']
-    following = result['following']
-    capy = f"""**Info Of {name}**
-**Username:** `{title}`
-**Bio:** `{bio}`
-**Profile Link:** [Click Here]({url})
-**Company:** `{company}`
-**Created On:** `{created_at}`
-**Repositories:** `{repositories}`
-**Blog:** `{blog}`
-**Location:** `{location}`
-**Followers:** `{followers}`
-**Following:** `{following}`
+    pablo = await message.reply_text("Processing...")
+    args = message.text.split(None, 1)[1]
+    if len(message.command) == 1:
+        await pablo.edit("No input found")
+        return
+    r = requests.get("https://api.github.com/search/repositories", params={"q": args})
+    lool = r.json()
+    if lool.get("total_count") == 0:
+        await pablo.edit("File not found")
+        return
+    else:
+        lol = lool.get("items")
+        qw = lol[0]
+        txt = f"""
+<b>Name :</b> <i>{qw.get("name")}</i>
 
-**@kinzanoufal**"""
-    await message.reply_text(text=title, reply_markup=BUTTONS)
-    await bot.send_message(LOG_CHANNEL, A.format(message.from_user.mention, message.from_user.id)) 
-    await k.delete()
-     
+<b>Full Name :</b> <i>{qw.get("full_name")}</i>
+
+<b>Link :</b> {qw.get("html_url")}
+
+<b>Fork Count :</b> <i>{qw.get("forks_count")}</i>
+
+<b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
+"""
+        if qw.get("description"):
+            txt += f'<b>Description :</b> <code>{qw.get("description")}</code>'
+
+        if qw.get("language"):
+            txt += f'<b>Language :</b> <code>{qw.get("language")}</code>'
+
+        if qw.get("size"):
+            txt += f'<b>Size :</b> <code>{qw.get("size")}</code>'
+
+        if qw.get("score"):
+            txt += f'<b>Score :</b> <code>{qw.get("score")}</code>'
+
+        if qw.get("created_at"):
+            txt += f'<b>Created At :</b> <code>{qw.get("created_at")}</code>'
+
+        if qw.get("archived") == True:
+            txt += f"<b>This Project is Archived</b>"
+        await pablo.edit(txt, disable_web_page_preview=True)
+
+
+
