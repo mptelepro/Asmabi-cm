6ecde3e3242d600289139611c718cbd83f802a7d
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-17 01:03:25 +0530

    Update Github.py

--- plugins/MODULES/Github.py
+++ plugins/MODULES/Github.py
@@ -21,51 +21,45 @@ A = """{} with user id:- {} used /git command."""
 
 
 @Client.on_message(filters.chat(-1001203428484) & filters.text & filters.command('repo'))
-# @Client.on_message(filters.command('repo') & filters.chat (SUPPORT_CHAT_ID))
-async def git(Kashmira, message):
-#    un = message.text
-#    search = https://github.com/search?q={un}+language%3APython&type=repositories&l=Python&s=updated&o=desc
+async def repo(Kashmira, message):
+    un = message.text.split(None, 1)[1]
+    search = https://github.com/search?q={un}+language%3APython&type=repositories&l=Python&s=updated&o=desc
     
     pablo = await message.reply_text("Processing...")
-    args = message.text.split(None, 1)[1]
-    if len(message.command) == 1:
-        await pablo.edit("No input found")
-        return
-    r = requests.get("https://api.github.com/search/repositories", params={"q": args})
-    lool = r.json()
-    if lool.get("total_count") == 0:
-        await pablo.edit("File not found")
-        return
-    else:
-        lol = lool.get("items")
-        qw = lol[0]
-        txt = f"""
+    args = message.text.split(None, 1)[1]
+    if len(message.command) == 1:
+        await pablo.edit("No input found")
+        return
+    r = requests.get("https://api.github.com/search/repositories", params={"q": args})
+    lool = r.json()
+    if lool.get("total_count") == 0:
+        await pablo.edit("File not found")
+        return
+    else:
+        lol = lool.get("items")
+        qw = lol[0]
+        txt = f"""
 <b>Name :</b> <i>{qw.get("name")}</i>
-
 <b>Full Name :</b> <i>{qw.get("full_name")}</i>
-
 <b>Link :</b> {qw.get("html_url")}
-
 <b>Fork Count :</b> <i>{qw.get("forks_count")}</i>
-
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 """
-        if qw.get("description"):
-            txt += f'<b>Description :</b> <code>{qw.get("description")}</code>'
+        if qw.get("description"):
+            txt += f'<b>Description :</b> <code>{qw.get("description")}</code>'
 
-        if qw.get("language"):
-            txt += f'<b>Language :</b> <code>{qw.get("language")}</code>'
+        if qw.get("language"):
+            txt += f'<b>Language :</b> <code>{qw.get("language")}</code>'
 
-        if qw.get("size"):
-            txt += f'<b>Size :</b> <code>{qw.get("size")}</code>'
+        if qw.get("size"):
+            txt += f'<b>Size :</b> <code>{qw.get("size")}</code>'
 
-        if qw.get("score"):
-            txt += f'<b>Score :</b> <code>{qw.get("score")}</code>'
+        if qw.get("score"):
+            txt += f'<b>Score :</b> <code>{qw.get("score")}</code>'
 
-        if qw.get("created_at"):
-            txt += f'<b>Created At :</b> <code>{qw.get("created_at")}</code>'
+        if qw.get("created_at"):
+            txt += f'<b>Created At :</b> <code>{qw.get("created_at")}</code>'
 
-        if qw.get("archived") == True:
-            txt += f"<b>This Project is Archived</b>"
-        await pablo.edit(txt, disable_web_page_preview=True)
-#        await pablo.edit(f" <a href='https://github.com/search?q={un}+language%3APython&type=repositories&l=Python&s=updated&o=desc'>total url</a> ", disable_web_page_preview=True)
+        if qw.get("archived") == True:
+            txt += f"<b>This Project is Archived</b>"
+        await pablo.edit(txt, disable_web_page_preview=True)
