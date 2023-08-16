727c35268035d2d4765053e15fa1406f96461e3a
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-16 14:01:21 +0530

    Update engine.py

--- plugins/helpers/engine.py
+++ plugins/helpers/engine.py
@@ -1,6 +1,7 @@
 import openai
 from info import OPENAI
 async def ai(query):
+    
     openai.api_key = OPENAI #Your openai api key
     response = openai.Completion.create(engine="text-davinci-002", prompt=query, max_tokens=100, n=1, stop=None, temperature=0.9, timeout=5)
     return response.choices[0].text.strip()
@@ -11,8 +12,10 @@ async def ask_ai(client, m, message):
         # Generate response using OpenAI API
         response = await ai(question)
         # Send response back to user
-        await m.edit(f"{response}")
+        await m.edit(f"user: {message.from_user.mention} \n{response}")
     except Exception as e:
         # Handle other errors
         error_message = f"An error occurred: {e}"
         await m.edit(error_message)
+    else:
+        await message.reply_text(😥 Sᴏʀʀʏ {message.from_user.mention}, \nYᴏᴜ Cᴀɴ'ᴛ Aꜱᴋ Qᴜᴇꜱᴛɪᴏɴꜱ Hᴇʀᴇ !!!\n/openai Cᴏᴍᴍᴀɴᴅ Oɴʟʏ Wᴏʀᴋ Oɴ Mʏ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ ♨️
