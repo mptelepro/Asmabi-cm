44606d3bdb467f26526b49dafd0e4d55c918ee59
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-16 13:48:40 +0530

    Update engine.py

--- plugins/helpers/engine.py
+++ plugins/helpers/engine.py
@@ -1,7 +1,7 @@
 import openai
 from info import OPENAI
 async def ai(query):
-    openai.api_key = "OPENAI" #Your openai api key
+    openai.api_key = OPENAI #Your openai api key
     response = openai.Completion.create(engine="text-davinci-002", prompt=query, max_tokens=100, n=1, stop=None, temperature=0.9, timeout=5)
     return response.choices[0].text.strip()
      
