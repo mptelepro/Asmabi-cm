6c0a4c37b62b590359aa76e5f95e39793b56ba40
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-17 20:17:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -24,8 +24,8 @@ async def start(client, message):
     content = message.text
     user = message.from_user.first_name
     user_id = message.from_user.id
-    lgcd = message.text.split("/chat")
-    lg_cd = lgcd[1].lower().replace(" ", "")
+#    lgcd = message.text.split("/chat")
+#    lg_cd = lgcd[1].lower().replace(" ", "")
     try:   
         if message.from_user.id == ADMIN: 
             await reply_text(client, message)
