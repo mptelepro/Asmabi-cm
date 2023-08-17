7fc8d2be544db4fe8f53eabf463a5c24192cf935
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-17 12:52:29 +0530

    Create Books.py

---
+++ plugins/Books.py
@@ -0,0 +1,14 @@
+from pyrogram.types import InputMediaPhoto, InputMediaVideo
+
+
+
+@Client.on_message(filters.command("book"))
+async def book(bot, message):
+await bot.send_media_group(
+    "me",
+    [
+        InputMediaPhoto("https://telegra.ph/file/442eabcd1bec22e3cca6f.jpg"),
+        InputMediaPhoto("https://telegra.ph/file/442eabcd1bec22e3cca6f.jpg", caption="photo caption"),
+        InputMediaVideo("https://telegra.ph/file/53e47d78206577914c487.mp4", caption="video caption")
+    ]
+)
