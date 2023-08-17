74bea567d609de46f141ec48ce093b34365d0ca4
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-17 22:30:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -52,8 +52,7 @@ async def start(client, message):
         )
         await asyncio.sleep(3000)
         await k.delete()
-    except Exception as e:
-        logger.exception(e)
+    
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
