ad3c40150c820b3fe524cf4160f92c903893260b
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-17 20:19:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -33,19 +33,8 @@ async def start(client, message):
         await client.send_message(
             chat_id=ADMIN,
             text=script.PM_TXT_ATT.format(reference_id, info.first_name, message.text),
-            parse_mode=enums.ParseMode.HTML,
-            reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton('🎁𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬🎁', url="http://t.me/nasrani_bot?startgroup=true")
-                            ],
-                            [
-                                InlineKeyboardButton('📩𝐑𝐄𝐐𝐔𝐀𝐒𝐓 𝐆𝐑𝐎𝐔𝐏📩', url="https://t.me/NasraniMovies"),
-                                InlineKeyboardButton('☘𝐍𝐄𝐖 𝐌𝐎𝐕𝐈𝐄𝐒☘', url="https://t.me/HDAZmovies")
-                            ]                            
-                        ]
-                    )
-                )
+            parse_mode=enums.ParseMode.HTML
+        )
         await asyncio.sleep(3000)
         await k.delete()
     except Exception as e:
