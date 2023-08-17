9e2059c7e237b43425a140445b98c9b05bcc2988
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-17 08:25:19 +0530

    Update Github.py

--- plugins/MODULES/Github.py
+++ plugins/MODULES/Github.py
@@ -33,6 +33,60 @@ START_MESSAGE = """
 
 """
 
+# (c) @KoshikKumar17
+import os
+import requests
+import pyrogram
+import json
+from info import LOG_CHANNEL
+from pyrogram import Client as Koshik
+from pyrogram import filters, enums
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, filters
+
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('✨ Made By ✨', url='https://t.me/nasrani_update')]])
+A = """{} with user id:- {} used /git command."""
+
+@Client.on_message(filters.command(["github", "git"]))
+async def getgithub(bot, message):
+    if len(message.command) != 2:
+        await message.reply_text("/github Username \n\n Like:- `/github hkrrish`", quote=True)
+        return
+    await message.reply_chat_action(enums.ChatAction.TYPING)
+    k = await message.reply_text("**Processing...⏳**", quote=True)    
+    un = message.text.split(None, 1)[1]
+    URL = f'https://api.github.com/users/{un}'
+    request = requests.get(URL)
+    result = request.json()
+    username = result['login']
+    url = result['html_url']
+    name = result['name']
+    company = result['company']
+    bio = result['bio']
+    created_at = result['created_at']
+    avatar_url = result['avatar_url']
+    blog = result['blog']
+    location = result['location']
+    repositories = result['public_repos']
+    followers = result['followers']
+    following = result['following']
+    capy = f"""**Info Of {name}**
+**Username:** `{username}`
+**Bio:** `{bio}`
+**Profile Link:** [Click Here]({url})
+**Company:** `{company}`
+**Created On:** `{created_at}`
+**Repositories:** `{repositories}`
+**Blog:** `{blog}`
+**Location:** `{location}`
+**Followers:** `{followers}`
+**Following:** `{following}`
+
+**@kinzanoufal**"""
+    await message.reply_photo(photo=avatar_url, caption=capy, reply_markup=BUTTONS)
+    await bot.send_message(LOG_CHANNEL, A.format(message.from_user.mention, message.from_user.id)) 
+    await k.delete()
+        
 
 
 
@@ -67,6 +121,8 @@ async def git(Kashmira, message):
 # <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 # """
         txt = f"""
+<b>𝐍𝐚𝐦𝐞 : </b> <i>{qw.get("login")}</i>
+        
 <b>𝐍𝐚𝐦𝐞 : </b> <i>{qw.get("name")}</i>
 
 <b>𝐅𝐮𝐥𝐥 𝐍𝐚𝐦𝐞 : </b> <i>{qw.get("full_name")}</i>
@@ -80,22 +136,22 @@ async def git(Kashmira, message):
 𝐓𝐨𝐭𝐚𝐥 𝐅𝐢𝐥𝐞𝐬 : <a href='https://github.com/search?q={args}+language%3APython&type=repositories&l=Python&s=updated&o=desc'>{args}</a>
 """        
         if qw.get("description"):
-            txt += f'<b>Description :</b> <code>{qw.get("description")}</code>'
+            txt += f'<b>Description :</b> <code>{qw.get("description")}</code> \n'
 
         if qw.get("language"):
-            txt += f'<b>Language :</b> <code>{qw.get("language")}</code>'
+            txt += f'<b>Language :</b> <code>{qw.get("language")}</code> \n'
 
         if qw.get("size"):
-            txt += f'<b>Size :</b> <code>{qw.get("size")}</code>'
+            txt += f'<b>Size :</b> <code>{qw.get("size")}</code> \n'
 
         if qw.get("score"):
-            txt += f'<b>Score :</b> <code>{qw.get("score")}</code>'
+            txt += f'<b>Score :</b> <code>{qw.get("score")}</code> \n'
 
         if qw.get("created_at"):
-            txt += f'<b>Created At :</b> <code>{qw.get("created_at")}</code>'
+            txt += f'<b>Created At :</b> <code>{qw.get("created_at")}</code> \n'
 
         if qw.get("archived") == True:
-            txt += f"<b>This Project is Archived</b>"
+            txt += f"<b>This Project is Archived</b> \n"
 #        await pablo.edit(txt, disable_web_page_preview=True)
     
 #    search = https://github.com/search?q={args}+language%3APython&type=repositories&l=Python&s=updated&o=desc
