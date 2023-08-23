ef66f2ea6b570fa7e99072eeaeecde0536784da7
MuraliNew <140889858+MuraliNew@users.noreply.github.com>
2023-08-23 22:55:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1192,7 +1192,7 @@ async def pm_text(client: Client, message):
 
 
 
-@Client.on_message(filters.private & filters.user(ADMIN) & filters.text & filters.reply) # filters.command("ok"))
+@Client.on_message(filters.private & filters.user(ADMIN) & filters.text) # filters.command("ok"))
 async def reply_text(client: Client, message):
     try:
         reference_id = True
